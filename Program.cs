4e67a0d849379c9fa477f7100a2c7efd38c7debe
Ramandeep Formay <ramandeepformay@gmail.com>
2020-10-03 16:16:17 -0600

    new files added

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace quiz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the age to get the best price for you :)");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age <= 5)
+            {
+                Console.WriteLine("Free");
+            }
+            else if (age > 5 && age < 14)
+            {
+                Console.WriteLine("Price is 7.99");
+            }
+            else if (age >= 14 && age < 65)
+            {
+                Console.WriteLine("Price is 11.99");
+            }
+            else
+            {
+                Console.WriteLine("9.99");
+            }
+
+        }
+    }
+}
