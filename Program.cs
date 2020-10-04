8f65bc44d13ab649cbfa70c3fd8480217fa01ee5
Ramandeep Formay <ramandeepformay@gmail.com>
2020-10-04 17:01:57 -0600

    new functionality added

--- Program.cs
+++ Program.cs
@@ -7,25 +7,70 @@ namespace quiz1
         static void Main(string[] args)
         {
 
+            TimeZone localZone = TimeZone.CurrentTimeZone;
+            DateTime currentDate = DateTime.Now;
+            string days = currentDate.DayOfWeek.ToString();
+
+
             Console.WriteLine("Enter the age to get the best price for you :)");
             int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Do you have a University Card? If yes enter yes or y");
+            string univCard = Console.ReadLine();
+
             if (age <= 5)
             {
-                Console.WriteLine("Free");
+                Console.WriteLine("Free Ticket");
             }
             else if (age > 5 && age < 14)
             {
-                Console.WriteLine("Price is 7.99");
+                if (days == "Tuesday")
+                {
+                    Console.WriteLine("Price of the ticket is 3.98");
+                }
+                else if (univCard == "yes" || univCard == "y")
+                {
+                    Console.WriteLine("Price of the ticket is 6.99");
+                }
+                else
+                {
+                    Console.WriteLine("Price of the ticket is 7.99");
+                }
             }
             else if (age >= 14 && age < 65)
             {
-                Console.WriteLine("Price is 11.99");
+                if (days == "Tuesday")
+                {
+                    Console.WriteLine("Price of the ticket is 5.98");
+                }
+                else if (univCard == "yes" || univCard == "y")
+                {
+                    Console.WriteLine("Price of the ticket is 10.99");
+                }
+                else
+                {
+
+                    Console.WriteLine("Price of the ticket is 11.99");
+                }
+
             }
             else
             {
-                Console.WriteLine("9.99");
+                if (days == "Tuesday")
+                {
+                    Console.WriteLine("Price of the ticket is 4.98");
+                }
+                else if (univCard == "yes" || univCard == "y")
+                {
+                    Console.WriteLine("Price of the ticket is 8.99");
+                }
+                else
+                {
+                    Console.WriteLine(" Price of the ticket is 9.99");
+                }
             }
-
         }
+
+
     }
 }
+
